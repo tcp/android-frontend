b1f01b41ea755c0f088bb6c5bbf9353d7b99fb39
thiago <thiagocostaporto@gmail.com>
2012-10-04 17:43:49 +0200

    asdf

---
+++ netinf.android.AndroidNetInfActivity/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/bin/AndroidManifest.xml : \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/AndroidManifest.xml \
---
+++ netinf.android.AndroidNetInfActivity/bin/AndroidNetInfActivity-debug-unaligned.apk.d
@@ -0,0 +1,15 @@
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/bin/AndroidNetInfActivity-debug-unaligned.apk : \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/bin/AndroidNetInfActivity.ap_ \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/bin/classes.dex \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/src/netinf/android/common/communication/protobuf/TransferringMessage.proto \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/log4j-1.2.16.jar \
+/home/tcp/Android SDK/android-sdk-linux/tools/support/annotations.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/httpmime-4.1.2.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/opennetif.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/guice-2.0-no_aop.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/javax.inject.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/org.restlet.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/commons-lang-2.3.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/commons-io-2.1.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/protobuf-java-2.3.0.jar \
+/home/tcp/workspace/netinf.android.AndroidNetInfActivity/libs/json-simple-1.1.1.jar \
