fdc92bf5e1cd8fae35de44e19132b6e6b49b9ffc
Kim-Anh Tran <tran.kimanh1988@gmail.com>
2012-10-22 04:03:03 +0200

    Refactoring.

--- LISA Application/src/project/cs/lisa/netinf/node/access/rest/resources/BOResource.java
+++ LISA Application/src/project/cs/lisa/netinf/node/access/rest/resources/BOResource.java
@@ -1,3 +1,46 @@
+/*
+ * Copyright (C) 2009-2011 University of Paderborn, Computer Networks Group
+ * (Full list of owners see http://www.netinf.org/about-2/license)
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Paderborn nor the names of its contributors may be used to endorse
+ *       or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+
+ * Uppsala University
+ *
+ * Project CS course, Fall 2012
+ *
+ * Projekt DV/Project CS, is a course in which the students develop software for
+ * distributed systems. The aim of the course is to give insights into how a big
+ * project is run (from planning to realization), how to construct a complex
+ * distributed system and to give hands-on experience on modern construction
+ * principles and programming methods.
+ *
+ * All rights reserved.
+ *
+ * Copyright (C) 2012 LISA team
+ */
+
 package project.cs.lisa.netinf.node.access.rest.resources;
 
 import java.io.FileNotFoundException;
@@ -16,6 +59,14 @@ import project.cs.lisa.transferdispatcher.TransferDispatcher;
 import android.os.Environment;
 import android.util.Log;
 
+/**
+ * 
+ * @author Hugo 
+ * @author Miguel
+ * @author Paolo Boshini
+ * @author Kim-Anh Tran
+ *
+ */
 public class BOResource extends LisaServerResource {
 
 	/** Debugging Tag. */
@@ -42,7 +93,31 @@ public class BOResource extends LisaServerResource {
 		mHashAlgorithm = getQuery().getFirstValue("HASH_ALG", true);
 	}
 
-
+	/*
+	 * Copyright (C) 2009-2011 University of Paderborn, Computer Networks Group
+	 * (Full list of owners see http://www.netinf.org/about-2/license)
+	 * 
+	 * All rights reserved.
+	 * 
+	 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+	 * 
+	 *     * Redistributions of source code must retain the above copyright notice,
+	 *       this list of conditions and the following disclaimer.
+	 *     * Redistributions in binary form must reproduce the above copyright notice,
+	 *       this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+	 *     * Neither the name of the University of Paderborn nor the names of its contributors may be used to endorse
+	 *       or promote products derived from this software without specific prior written permission.
+	 * 
+	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+	 * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	 * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+	 * SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+	 * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	 * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+	 * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	 * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 */
 	@Get
 	public HashMap<String, String> retrieveBO() {
 
