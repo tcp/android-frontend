4cbc50cf2950bb846d97017c279330d7670333d1
paolo <boschini.paolo@gmail.com>
2012-10-30 15:45:42 +0100

    added progressbar

--- Prototype/src/com/example/prototype/MainActivity.java
+++ Prototype/src/com/example/prototype/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +14,9 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Set progress bar
+        ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
     }
 
     @Override
