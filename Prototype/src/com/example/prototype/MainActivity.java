27ac709c7b7107485caa00cf8e0dd5132dbc9364
Linus Sunde <lejums@gmail.com>
2012-10-30 14:43:06 +0100

    settings and help screens

--- Prototype/src/com/example/prototype/MainActivity.java
+++ Prototype/src/com/example/prototype/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.prototype;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.app.Activity;
 import android.view.Menu;
 
@@ -10,6 +11,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        getActionBar().setDisplayShowHomeEnabled(false);
+        getActionBar().setDisplayUseLogoEnabled(false);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        // Display the settings fragment as the main content.
+        setTitle("Settings");
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new XmlFragment(R.xml.preferences)).commit();
+        
+        
+        // Display the help fragment as the main content.
+//        setTitle("Help");
+//        getFragmentManager().beginTransaction().replace(android.R.id.content, new XmlFragment(R.xml.help)).commit();
+        
     }
 
     @Override
@@ -17,4 +32,23 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public static class XmlFragment extends PreferenceFragment {
+        
+        private int mXml;
+        
+        public XmlFragment(int xml) {
+            mXml = xml;
+        }
+        
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(mXml);
+        }
+
+    }
+    
 }
