bee9910a5fc4ee24bf09ed05edc67a550059e3a3
paolo <boschini.paolo@gmail.com>
2012-11-05 15:59:07 +0100

    merged main files.

--- LISA Application/src/project/cs/lisa/application/MainNetInfActivity.java
+++ LISA Application/src/project/cs/lisa/application/MainNetInfActivity.java
@@ -1,20 +1,4 @@
 /**
-
- * Uppsala University
- *
- * Project CS course, Fall 2012
- *
- * Projekt DV/Project CS, is a course in which the students develop software for
- * distributed systems. The aim of the course is to give insights into how a big
- * project is run (from planning to realization), how to construct a complex
- * distributed system and to give hands-on experience on modern construction
- * principles and programming methods.
- *
- * All rights reserved.
- *
- * Copyright (C) 2012 LISA team
- */
-/**
  * Copyright 2012 Ericsson, Uppsala University
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
