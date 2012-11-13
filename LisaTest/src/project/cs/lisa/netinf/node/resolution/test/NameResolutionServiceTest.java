b90f3392023224933efd090adc925c55fe9149a5
harodani <harodani@gmail.com>
2012-11-13 10:43:59 +0100

    Commiting testing for put and get

--- LisaTest/src/project/cs/lisa/netinf/node/resolution/test/NameResolutionServiceTest.java
+++ LisaTest/src/project/cs/lisa/netinf/node/resolution/test/NameResolutionServiceTest.java
@@ -6,6 +6,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import netinf.common.datamodel.DatamodelFactory;
+import netinf.common.datamodel.Identifier;
+import netinf.common.datamodel.IdentifierLabel;
+import netinf.common.datamodel.InformationObject;
+import netinf.common.datamodel.attribute.Attribute;
 import netinf.common.datamodel.impl.DatamodelFactoryImpl;
 
 import org.apache.http.Header;
@@ -15,6 +19,9 @@ import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.entity.mime.MultipartEntity;
 
 
+import project.cs.lisa.netinf.common.datamodel.SailDefinedAttributeIdentification;
+import project.cs.lisa.netinf.common.datamodel.SailDefinedAttributePurpose;
+import project.cs.lisa.netinf.common.datamodel.SailDefinedLabelName;
 import project.cs.lisa.netinf.node.resolution.NameResolutionService;
 
 import android.test.AndroidTestCase;
@@ -124,6 +131,77 @@ public class NameResolutionServiceTest extends AndroidTestCase {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+	}
+	
+	public void testPut() {
+		String hashAlg = "sha-256";
+		String hash = "asdas";
+		String contentType = "plain/text";
+		String bluetoothMac = "12:CD";
+		
+		Identifier identifier = mDatamodelFactory.createIdentifier();
+	      
+        //Creating the HASH_ALG label
+        IdentifierLabel identifierLabel = mDatamodelFactory.createIdentifierLabel();
+        identifierLabel.setLabelName(SailDefinedLabelName.HASH_ALG.getLabelName());
+        identifierLabel.setLabelValue(hashAlg);
+        identifier.addIdentifierLabel(identifierLabel);
+    
+        //Creating the HASH_CONTENT label
+        IdentifierLabel identifierLabel2 = mDatamodelFactory.createIdentifierLabel();
+        identifierLabel2.setLabelName(SailDefinedLabelName.HASH_CONTENT.getLabelName());
+        identifierLabel2.setLabelValue(hash);
+        identifier.addIdentifierLabel(identifierLabel2);
+        
+        IdentifierLabel identifierLabel3 = mDatamodelFactory.createIdentifierLabel();
+        identifierLabel3.setLabelName(SailDefinedLabelName.CONTENT_TYPE.getLabelName());
+        identifierLabel3.setLabelValue(contentType);
+        identifier.addIdentifierLabel(identifierLabel3);
+        
+        InformationObject io = mDatamodelFactory.createInformationObject();
+        io.setIdentifier(identifier);
+       
+        Attribute address = mDatamodelFactory.createAttribute();
+        address.setAttributePurpose(SailDefinedAttributePurpose.LOCATOR_ATTRIBUTE.toString());
+        address.setIdentification(SailDefinedAttributeIdentification.BLUETOOTH_MAC.getURI());
+        address.setValue(bluetoothMac);
+        io.addAttribute(address);   
+        
+        mNameResolutionService.put(io);
+       
+        
+	}
+	
+	public void testGet() {
+		Identifier identifier = mDatamodelFactory.createIdentifier();
+		
+		String hashAlg = "sha-256";
+		String hash = "asdas";
+	      
+        //Creating the HASH_ALG label
+        IdentifierLabel identifierLabel = mDatamodelFactory.createIdentifierLabel();
+        identifierLabel.setLabelName(SailDefinedLabelName.HASH_ALG.getLabelName());
+        identifierLabel.setLabelValue(hashAlg);
+        identifier.addIdentifierLabel(identifierLabel);
+    
+        //Creating the HASH_CONTENT label
+        IdentifierLabel identifierLabel2 = mDatamodelFactory.createIdentifierLabel();
+        identifierLabel2.setLabelName(SailDefinedLabelName.HASH_CONTENT.getLabelName());
+        identifierLabel2.setLabelValue(hash);
+        identifier.addIdentifierLabel(identifierLabel2);
+        
+        
+		InformationObject io = mDatamodelFactory.createInformationObject();
+		
+		io = mNameResolutionService.get(identifier);
+		
+		//Assert content type is not null
+		assertNotNull(io.getIdentifier().getIdentifierLabel(SailDefinedLabelName.CONTENT_TYPE.getLabelName()));
+		//Assert meta-data is not null
+		assertNotNull(io.getIdentifier().getIdentifierLabel("metadata"));
+		
 		
 		
 	}
