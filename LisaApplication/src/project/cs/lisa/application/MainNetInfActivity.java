46af18bba5fdf5f537b0bf31b1ce041a06b968dd
Thiago Costa Porto <thiagocostaporto@gmail.com>
2012-11-16 11:12:46 +0100

    MainNetInfActivity:   -- Removed makeMsgId function.
    Signed-off-by: Thiago Costa Porto <thiagocostaporto@gmail.com>

--- LisaApplication/src/project/cs/lisa/application/MainNetInfActivity.java
+++ LisaApplication/src/project/cs/lisa/application/MainNetInfActivity.java
@@ -263,24 +263,6 @@ public class MainNetInfActivity extends Activity {
         //        startActivity(intent);
     }
 
-    private String makeMsgId() {
-        final TelephonyManager tm = (TelephonyManager) getBaseContext().getSystemService(Context.TELEPHONY_SERVICE);
-
-        final String tmDevice, tmSerial, androidId;
-        tmDevice = "" + tm.getDeviceId();
-        tmSerial = "" + tm.getSimSerialNumber();
-        androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
-
-        UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
-        String deviceId = deviceUuid.toString();
-        
-        int randomNumber = new Random(System.currentTimeMillis()).nextInt();
-        
-        String msgId = deviceId + String.valueOf(randomNumber);
-        
-        return msgId;
-    }
-
     /**
      * Creates an intent to select an image from the gallery.
      * @param v The view that fired this event.
